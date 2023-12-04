5b76b950410fcad85e4ecedd3b1fa00be7747001
moatazMahdi <moataz.mahdi@contact.eg>
2023-12-04 15:08:19 +0200

    mmmm

--- src/navegations/Routes.tsx
+++ src/navegations/Routes.tsx
@@ -1,4 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+import * as ReactDOM from "react-dom";
 import Home from "../pages/home/home";
 import About from "../pages/About/About";
 import ContactUs from "../pages/ContactUs/ContactUs";
@@ -14,25 +21,41 @@ import Detalis from "../pages/service/Detalis";
 import ScrollToTop from "../utils/ScrollToTop";
 
 const RoutesNavegation = () => {
-  return (
-    <BrowserRouter basename="/">
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="contact" element={<ContactUs />} />
-        <Route path="/bolg" element={<Bolg />} />
-        <Route path="/digitalContent" element={<DigitalContent />} />
-        <Route path="/innovation" element={<Innovation />} />
-        <Route path="/works" element={<OurWork />} />
-        <Route path="/productDevelopment" element={<ProductDevelopment />} />
-        <Route path="/strategy" element={<Strategy />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/service" element={<Service />} />
-        <Route path="/service/:cardId" element={<Detalis />} />
-      </Routes>
-    </BrowserRouter>
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <Home />,
+      children: [
+        {
+          path: "about",
+          element: <About />,
+        },
+      ],
+    },
+  ]);
+
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <RouterProvider router={router} />
   );
+  // return (
+  //   <createHashRouter basename="/">
+  //     <ScrollToTop />
+  //     <Routes>
+  //       <Route path="/" element={<Home />} />
+  //       <Route path="/about" element={<About />} />
+  //       <Route path="contact" element={<ContactUs />} />
+  //       <Route path="/bolg" element={<Bolg />} />
+  //       <Route path="/digitalContent" element={<DigitalContent />} />
+  //       <Route path="/innovation" element={<Innovation />} />
+  //       <Route path="/works" element={<OurWork />} />
+  //       <Route path="/productDevelopment" element={<ProductDevelopment />} />
+  //       <Route path="/strategy" element={<Strategy />} />
+  //       <Route path="/team" element={<Team />} />
+  //       <Route path="/service" element={<Service />} />
+  //       <Route path="/service/:cardId" element={<Detalis />} />
+  //     </Routes>
+  //   </createHashRouter>
+  // );
 };
 
 export default RoutesNavegation;
